f0fdc549e7eeadfd4f308e12bc47df45ed2faf45
fukurin <fukurin@ucl.nuee.nagoya-u.ac.jp>
2021-08-05 19:31:50 +0900

    add ros2 configure

---
+++ ros2/build.sh
@@ -0,0 +1,4 @@
+docker build\
+     --rm \
+     --tag fukurin/ros2:galactic-base \
+     --file Dockerfile.ros2.gui .
---
+++ ros2/run.sh
@@ -0,0 +1,9 @@
+xhost +local:root
+docker run \
+    -it --rm \
+    --gpus all \
+    --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --volume=$HOME/.Xauthority:$HOME/.Xauthority:rw \
+    --env="XAUTHORITY=$HOME/.Xauthority" \
+    --env="DISPLAY=${DISPLAY}" \
+    fukurin/ros2:galactic-base
