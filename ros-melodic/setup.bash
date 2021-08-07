e3a220387f830533380d9c0d8376e2882fca75f0
fukurin <fukurin@ucl.nuee.nagoya-u.ac.jp>
2021-08-07 12:00:52 +0900

    update

---
+++ ros-melodic/setup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export HOME="/home/$USERNAME/"
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
---
+++ ros2-galactic/setup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export HOME="/home/developer"
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
